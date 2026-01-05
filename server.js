0c94e0864ebfa264035f048dfdb3acc25e63f131
xooron <107441167+xooron@users.noreply.github.com>
2026-01-05 21:58:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
 const cors = require('cors');
 
 const app = express();
@@ -23,14 +22,11 @@ io.on('connection', (socket) => {
         if (!db.users[data.id]) {
             db.users[data.id] = { 
                 id: data.id, name: data.name, username: data.username, avatar: data.avatar, 
-                balance: 100.0, refCount: 0, refPending: 0.00, refTotal: 0.00, referredBy: data.refBy 
+                balance: 100.0, refCount: 0, refPending: 0.0, refTotal: 0.0, referredBy: data.refBy 
             };
-            if (data.refBy && db.users[data.refBy]) {
-                db.users[data.refBy].refCount++;
-            }
         }
-        socket.emit('update_data', db.users);
-        io.emit('update_arena', { players, totalBank });
+        io.emit('update_data', db.users);
+        socket.emit('update_arena', { players, totalBank });
     });
 
     socket.on('place_bet', (data) => {
@@ -45,27 +41,19 @@ io.on('connection', (socket) => {
             if (p) p.amount += amount;
             else players.push({ id: u.id, username: u.username, avatar: u.avatar, amount, color: COLORS[players.length % COLORS.length] });
             
-            calculateChances();
+            totalBank = players.reduce((s, p) => s + p.amount, 0);
+            players.forEach(p => p.chance = (p.amount / totalBank) * 100);
+
             io.emit('update_data', db.users);
             io.emit('update_arena', { players, totalBank });
             if (players.length >= 2 && gameStatus === 'waiting') startCountdown();
         }
     });
 
-    socket.on('claim_ref', (data) => {
-        const u = db.users[data.id];
-        if (u && u.refPending > 0) {
-            u.balance += u.refPending;
-            u.refTotal += u.refPending;
-            u.refPending = 0;
-            io.emit('update_data', db.users);
-        }
-    });
-
     socket.on('request_winner', (data) => {
         if (gameStatus !== 'running') return;
         gameStatus = 'calculating';
-        
+
         let curX = 0;
         let winner = players[0];
         for (let p of players) {
@@ -76,20 +64,19 @@ io.on('connection', (socket) => {
         
         const fee = totalBank * 0.05;
         const winAmount = totalBank - fee;
-        const winnerBet = winner.amount;
         db.users[winner.id].balance += winAmount;
 
+        // Рефка
         players.forEach(p => {
             const user = db.users[p.id];
             if (user.referredBy && db.users[user.referredBy]) {
-                const refBonus = (p.amount * 0.05) * 0.10; 
-                db.users[user.referredBy].refPending += refBonus;
+                db.users[user.referredBy].refPending += (p.amount * 0.05) * 0.1;
             }
         });
         
-        io.emit('announce_winner', { winner, bank: winAmount, winnerBet });
+        io.emit('announce_winner', { winner, bank: winAmount, winnerBet: winner.amount });
         io.emit('update_data', db.users);
-        setTimeout(resetGame, 3000); // 3 секунды на показ победителя и очистка
+        setTimeout(resetGame, 4000);
     });
 });
 
@@ -107,8 +94,8 @@ function startGame() {
     gameStatus = 'running';
     io.emit('game_status', { status: 'running' });
     io.emit('start_game_sequence', {
-        startX: Math.random() * 200 + 50, 
-        startY: Math.random() * 200 + 50
+        startX: Math.random() * 150 + 75, 
+        startY: Math.random() * 150 + 75
     });
 }
 
@@ -118,9 +105,4 @@ function resetGame() {
     io.emit('game_status', { status: 'waiting' });
 }
 
-function calculateChances() {
-    totalBank = players.reduce((s, p) => s + p.amount, 0);
-    players.forEach(p => p.chance = (p.amount / totalBank) * 100);
-}
-
-server.listen(3000, () => console.log('Arena Server Started'));
+server.listen(3000, () => console.log('Server running on port 3000'));
